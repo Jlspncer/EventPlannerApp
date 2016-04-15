180da7a0f37bc9060a49657acc0ec84382cb1c4f
onu-tim-martin <t-martin@onu.edu>
2016-04-14 21:17:15 -0400

    Added files via upload

---
+++ Event Scheduler/Event Scheduler Tests/UnitTest1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Event_Scheduler;
+
+namespace Event_Scheduler_Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void ActivityConstructorTest()
+        {
+            Activity test = new Activity("test", 0, 50);
+            Assert.AreEqual("test", test.getName());
+            Assert.AreEqual(0, test.getStartTime());
+            Assert.AreEqual(50, test.getDuration());
+        }
+        public void GetEndTimeTest()
+        {
+            Activity test = new Activity("test", 7, 50);
+            Assert.AreEqual(57, test.getEndTime());
+        }
+
+    }
+}
---
+++ Event Scheduler/Event Scheduler/Activity.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Event_Scheduler
+{
+    public class Activity
+    {
+        string name;
+        double startTime;
+        double duration;
+        public Activity(string name, double startTime, double duration)
+        {
+            this.name = name;
+            this.startTime = startTime;
+            this.duration = duration;
+        }
+        public string getName() { return name; }
+        public double getStartTime() { return startTime; }
+        public double getDuration() { return duration; }
+        public double getEndTime()
+        {
+            return 0;
+        }
+    }
+}
